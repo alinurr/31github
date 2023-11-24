02dd90bc1ba3e9a2a76df8eb2ebc031c417f1a34
alinurr <alinurraiymbekova@gmail.com>
2023-11-24 09:06:22 +0600

    OOP Override

---
+++ OOP/Override/Car.java
@@ -0,0 +1,8 @@
+package OOP.Override;
+
+public class Car extends Vehicle{
+    @Override
+    void displayInfo(){
+        System.out.println("Cars are Four Wheeler");
+    }
+}
---
+++ OOP/Override/Vehicle.java
@@ -0,0 +1,19 @@
+package OOP.Override;
+
+public class Vehicle {
+    void displayInfo(){
+        System.out.println("Four Wheeler or Two Wheeler");
+    }
+
+    public static void main(String[] ags){
+
+        Car c = new Car();
+        c.displayInfo();
+    }
+}
+
+
+
+
+
+
